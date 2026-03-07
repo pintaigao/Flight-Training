52e8cf0fbc1d8f335f8e80c404ac2b19c9606833
Pintaigao He <pintaigao@gmail.com>
2026-03-07 13:45:03 -0600

    refactor(ui): tailwind global shell layout

--- src/components/layout/PrivateLayout.tsx
+++ src/components/layout/PrivateLayout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../sidebar/Sidebar';
-import './PrivateLayout.scss';
 
 export default function PrivateLayout() {
   return (
-    <div className="app-layout">
-      <Sidebar />
-      <main className="app-main">
+    <div className="min-h-screen lg:grid lg:grid-cols-[280px_1fr]">
+      <div className="lg:sticky lg:top-0 lg:h-screen">
+        <Sidebar />
+      </div>
+      <main className="p-4 sm:p-6 lg:p-8">
+        <div className="mx-auto w-full max-w-6xl">
         <Outlet />
+        </div>
       </main>
     </div>
   );
